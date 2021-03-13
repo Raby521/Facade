b9182f08d8e53575d9c673b450040d2382f6a05e
Raby521 <44761447+Raby521@users.noreply.github.com>
2021-03-13 20:39:38 +0800

    Add files via upload

---
+++ Facade/src/main/java/com/mycompany/facade/InterestCalculation.java
@@ -0,0 +1,8 @@
+package com.mycompany.facade;
+
+public interface InterestCalculation {
+    //interface by default is publicly accessible
+
+    public double calculateInterest(double accountBalance); 
+    
+}
