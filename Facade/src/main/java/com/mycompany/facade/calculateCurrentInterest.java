2a872feaad762bc5845b7a3776d7433d869a57ad
Raby521 <44761447+Raby521@users.noreply.github.com>
2021-03-13 20:35:56 +0800

    Delete Facade directory

--- Facade/src/main/java/com/mycompany/facade/calculateCurrentInterest.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.facade;
-
-public class calculateCurrentInterest implements InterestCalculation {
-
-    @Override
-    public double calculateInterest(double accountBalance) {
-        return accountBalance * (0.02 / 12);
-    }
-}
--- Facade/src/main/java/com/mycompany/facade/calculateSavingsInterest.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.facade;
-
-public class calculateSavingsInterest implements InterestCalculation {
-
-    @Override
-    public double calculateInterest(double accountBalance) {
-        return accountBalance * (0.04 / 12);
-    }
-}
